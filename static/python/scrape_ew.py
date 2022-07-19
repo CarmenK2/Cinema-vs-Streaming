46811586c473e954752c1ec07945da4adc349bd8
e'ddddddddddd <98539748+Ejwpls@users.noreply.github.com>
2022-07-19 20:40:13 +0800

    220719

--- static/python/scrape_ew.py
+++ static/python/scrape_ew.py
@@ -9,22 +9,41 @@ import requests
 
 # %%
 #Scrape usign request to get html of page
-page = requests.get('https://www.boxofficemojo.com/year/?ref_=bo_nb_di_secondarytab')
-b = page.content
 
-# Using Pandas to convert html into dataframe
-df_list = pd.read_html(b)
-df = df_list[-1]
+
+# Scrape for Boxoffice data and return dataframe
+def scrape_boxoffice():
+    page = requests.get('https://www.boxofficemojo.com/year/?ref_=bo_nb_di_secondarytab')
+    b = page.content
+
+    # Using Pandas to convert html into dataframe
+    df_list = pd.read_html(b)
+    df = df_list[-1]
 
 
 # Rename column name to be more accessible
-df = df.rename(columns={"Total Gross":"Total_gross", "%± LY":"LY","#1 Release":"No1_release"})
+    df = df.rename(columns={"Total Gross":"Total_gross", "%± LY":"LY","#1 Release":"No1_release"})
+
+    return df
 
 # Save JSON to resources folder
 # df.reset_index().to_json("../../Resources/box_office_scrape.json",orient='records')
 
+#Subscriber data to df 
+def df_Nextlixsubs():
+    df = pd.read_excel("../../Resources/Netflix_subscribers_AP_2013-2020.xlsx")
+    return df
 
-df1 = pd.read_excel("../../Resources/Netflix_subscribers_AP_2013-2020.xlsx")
+def df_Cinemadata():
+    df = pd.read_csv("../../Resources/cinema_data.csv")
+    return df
+
+def df_Streamingfee():
+    df = pd.read_csv("../../Resources/scrap_streaming_fee.csv")
+    return df  
+
+#%% 
+
 
 
 
